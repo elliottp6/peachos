e94b879eafd34d515a91d36fd14fddea44ed3cee
Elliott Prechter <elliott.prechter@gmail.com>
2022-03-24 21:11:57 -0400

    lecture 51 part 1: new string functions

--- src/string/string.c
+++ src/string/string.c
@@ -1,4 +1,7 @@
 #include "string.h"
+#include <stdint.h>
+
+char tolower( char c ) { return c >= 65 && c <= 90 ? c + 32 : c; }
 
 int strlen( const char* p ) {
     int i = 0;
@@ -10,6 +13,30 @@ int strnlen( const char* p, int max ) {
     int i;
     for( i = 0; i <= max && p[i]; i++ );
     return i;
+}
+
+int strnlen_terminator( const char* s, int max, char terminator ) {
+    int i;
+    for( i = 0; i < max && s[i] && s[i] != terminator; i++ );
+    return i;
+}
+
+int istrncmp( const char* s1, const char* s2, int len ) { // case-insensitive version of strncmp
+    uint8_t *p1 = (uint8_t*)s1, *p2 = (uint8_t*)s2;
+    for( int i = 0; i < len; i++ ) {
+        if( tolower( p1[i] ) != tolower( p2[i] ) ) return (int)p1[i] - (int)p2[i];
+        if( !p1[i] ) break;
+    }
+    return 0;
+}
+
+int strncmp( const char* s1, const char* s2, int len ) {
+    uint8_t *p1 = (uint8_t*)s1, *p2 = (uint8_t*)s2;
+    for( int i = 0; i < len; i++ ) {
+        if( p1[i] != p2[i] ) return (int)p1[i] - (int)p2[i];
+        if( !p1[i] ) break;
+    }
+    return 0;
 }
 
 char* strcpy( char* dest, const char* src ) {
--- src/string/string.h
+++ src/string/string.h
@@ -4,5 +4,9 @@
 int strlen( const char* p );
 int strnlen( const char* p, int max );
 char* strcpy( char* dest, const char* src );
+int strncmp( const char* s1, const char* s2, int len );
+int istrncmp( const char* s1, const char* s2, int len );
+int strnlen_terminator( const char* s, int max, char terminator );
 bool is_digit( char c );
 int to_numeric_digit( char c );
+char tolower( char c );
