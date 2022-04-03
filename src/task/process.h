101133a2c74a0a1850af1ea0729312f4c24b95ae
Elliott Prechter <elliott.prechter@gmail.com>
2022-04-03 17:23:19 -0400

    lecture 67 & 68: process header file

---
+++ src/task/process.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <stdint.h>
+#include "config.h"
+#include "task.h"
+
+struct process {
+    uint16_t id;
+    char filename[PEACHOS_MAX_PATH];
+    struct task* task;
+    void* allocations[PEACHOS_MAX_PROGRAM_ALLOCATIONS]; // memory allocations of the process
+    void *ptr, *stack; // the physical pointer to the process memory, stack memory
+    uint32_t size; // size of data pointed to by 'ptr'  
+};
+
+// functions
+int process_load_for_slot( const char* filename, struct process** process, int process_slot );
